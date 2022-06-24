708d447b267643c0c93ada1f34108264082259c2
jjbuell-dev <84111581+jjbuell-dev@users.noreply.github.com>
2022-06-24 15:59:05 -0600

    Initial project setup

---
+++ GetThingsDone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+// ViewController.swift
+// GetThingsDone
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
